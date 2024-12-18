1134b2b1d25c91495fd3e2aa6bc4e4bdc179e4e4
Bobbaru <bobbaru_krueger@yahoo.com>
2024-12-18 22:26:48 +0200

    misc: page title component refactoring

--- webapp/src/app/lists/page.tsx
+++ webapp/src/app/lists/page.tsx
@@ -1,31 +1,13 @@
 import { PageTitle } from "@/components/page-title";
-import { Button } from "@/components/ui/button";
 import { currentUser } from "@/features/auth/lib/auth";
 import { UserLists } from "@/features/lists/components";
-import Link from "next/link";
-import { IoAddCircleOutline } from "react-icons/io5";
 
 const ListsPage = async () => {
   const user = await currentUser();
 
   return (
     <div className="container space-y-6">
-      <PageTitle
-        label={`${user?.name}'s lists`}
-        button={
-          <Button
-            variant={"outline"}
-            size={"sm"}
-            className="flex gap-2"
-            asChild
-          >
-            <Link href={"/lists/add"}>
-              <IoAddCircleOutline size={18} />
-              Add List
-            </Link>
-          </Button>
-        }
-      />
+      <PageTitle label={`${user?.name}'s lists`} addBtnHref="/lists/add" />
       <UserLists />
     </div>
   );
--- webapp/src/components/page-title.tsx
+++ webapp/src/components/page-title.tsx
@@ -1,15 +1,39 @@
-import { ReactNode } from "react";
+import Link from "next/link";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { Button } from "./ui/button";
 
 interface Props {
   label: string;
-  button?: ReactNode;
+  addBtnHref?: string;
+  backBtnHref?: string;
 }
 
-export const PageTitle = ({ label, button }: Props) => {
+export const PageTitle = ({ label, addBtnHref, backBtnHref }: Props) => {
   return (
     <div className="flex flex-wrap items-center justify-between gap-4">
       <h1 className="text-heading1">{label}</h1>
-      {button}
+
+      {addBtnHref && (
+        <Button variant={"outline"} size={"sm"} className="flex gap-2" asChild>
+          <Link href={addBtnHref}>
+            <IoAddCircleOutline size={18} />
+            Add
+          </Link>
+        </Button>
+      )}
+      {backBtnHref && (
+        <Button
+          asChild
+          className="flex items-center justify-center gap-2"
+          variant={"outline"}
+        >
+          <Link href={backBtnHref}>
+            <IoMdArrowRoundBack size={18} />
+            <span className="hidden md:inline">Back</span>
+          </Link>
+        </Button>
+      )}
     </div>
   );
 };
