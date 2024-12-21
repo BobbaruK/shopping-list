99e24b6b47467649d6612df4742171331c96b131
Bobbaru <bobbaru_krueger@yahoo.com>
2024-12-22 01:02:32 +0200

    lists: create list page

---
+++ webapp/src/app/lists/[listId]/page.tsx
@@ -0,0 +1,28 @@
+import { PageTitle } from "@/components/page-title";
+import { currentUser } from "@/features/auth/lib/auth";
+import { getList } from "@/features/lists/data";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    listId: string;
+  };
+}
+
+const ListPage = async ({ params: { listId } }: Props) => {
+  const user = await currentUser();
+  const list = await getList(listId);
+
+  if (!list || list.createdUserId !== user?.id) notFound();
+
+  return (
+    <div className="container space-y-6">
+      <PageTitle label={list.name} backBtnHref="/lists" />
+      <div>
+        <pre>{JSON.stringify(list, null, 2)}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default ListPage;
